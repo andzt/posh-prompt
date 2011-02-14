f090a620b180c5dbc2be09a5d851d58912f74c4d
Julien <littleboy@users.sourceforge.net>
2011-02-14 14:07:34 -0500

    Update psm1 file and move install.ps1 to root directory

--- posh-prompt.psm1
+++ posh-prompt.psm1
@@ -1,11 +1,24 @@
 Push-Location $psScriptRoot
+.\git\CheckVersion.ps1 > $null
+
 . ./Settings.ps1
+. ./git/GitUtils.ps1
+. ./git/GitPrompt.ps1
+. ./git/GitTabExpansion.ps1
+. ./git/TortoiseGit.ps1
+. ./git/Utils.ps1
 . ./hg/HgUtils.ps1
 . ./hg/HgPrompt.ps1
 . ./hg/HgTabExpansion.ps1
 Pop-Location
 
 Export-ModuleMember -Function @(
+  'Write-GitStatus', 
+  'Get-GitStatus', 
+  'Enable-GitColors', 
+  'Get-GitDirectory',
+  'GitTabExpansion',
+  'tgit'
   'Write-HgStatus',
   'Get-HgStatus',
   'HgTabExpansion',
